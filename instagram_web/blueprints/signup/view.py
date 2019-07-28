d55357520628edce7dc8218822b068ea3d250bae
rafidah <alias_rafidah@yahoo.com>
2019-07-28 12:59:54 +0800

    sfhsfhsb

--- instagram_web/blueprints/signup/view.py
+++ instagram_web/blueprints/signup/view.py
@@ -36,7 +36,7 @@ def create_signup():
 
     name.save()
     # return redirect(url_for('home'))
-    return render_template("my_profile.html")
+    return render_template("signin.html")
     # return f'signin'
 
   
